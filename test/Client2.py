46d696536c7def2a9c88e7a2ee0a3f410f7b7acf
Eoryeong <43430856+Eoryeong@users.noreply.github.com>
2018-10-11 13:41:38 +0900

    Client end
    qyqy

--- test/Client2.py
+++ test/Client2.py
@@ -1,3 +1,34 @@
+import socket
+form threding import Thread
+
+HOST = '192.168.0.6'
+PORT = 7777
+
+def rcvMsg(sock):
+  while True:
+    try:
+      data = sock.recv(1024)
+      if not data:
+        break
+      print(data.decode())
+    except:
+      pass
+    
+def runChat():
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    t = Thread(target=rcvMsg, args=(sock,))
+    t.daemon = True
+    t.start()
+    
+    while True:
+      msg = input()
+      if msg == '/quit':
+        sock.send(msg.encode())
+        break
+        
+      sock.send(msg.encode())
+runChat()
 # import socket,sys
 #
 # HOST = '192.168.0.6'
